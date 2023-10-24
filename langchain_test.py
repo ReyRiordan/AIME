4487966cd89061370711708ce6e44e5dc49d44f7
ahmedali6395 <ahmedali6395@gmail.com>
2023-10-24 01:47:18 -0400

    10-24
    Added token counter, adding prompt directly from a file, etc. See comments/new files

--- langchain_test.py
+++ langchain_test.py
@@ -2,14 +2,26 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
+import textract
+import tiktoken
+import token_counter
+
 KEY = "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"
 MODEL = "gpt-4"
 
 llm = ChatOpenAI(openai_api_key=KEY, model_name=MODEL)
 conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
 
-user_input = input("START: ")
+
+# A list of all messages sent thusfar
+allMessages = []
+#Adding the prompt from the .docx file before the conversation begins
+user_input = textract.process("./Prompt/Chat_GPT_prompt_V1023_OBJ1.docx").decode()
+allMessages.append(user_input)
+
 while user_input != "END":
     print("GPT:" + conversation.predict(input=user_input) + "\n")
     user_input = input("User: ")
-
+    allMessages.append(user_input)
+    #Count number of tokens used
+    print("no. tokens used: "+str(token_counter.num_tokens_used(allMessages)))
---
+++ token_counter.py
@@ -0,0 +1,18 @@
+import textract
+import tiktoken
+
+def num_tokens_used(listOfMessages,encoding_name = "cl100k_base"):
+    """Counts number of tokens from list of input
+
+    Args:
+        listOfMessages (list of str): List of all messages passed to API
+        encoding_name (str): Preferred encoding type. Defaults to "cl100k_base".
+
+    Returns:
+        int: Number of tokens used
+    """
+    toReturn = 0
+    for message in listOfMessages:
+        encoding = tiktoken.get_encoding(encoding_name)
+        toReturn=toReturn+len(encoding.encode(message))
+    return toReturn

56b2fb7fd64f944fcd392d0fed7950184f59b402
ahmedali6395 <ahmedali6395@gmail.com>
2023-10-29 01:39:58 -0400

    working logger

--- langchain_test.py
+++ langchain_test.py
@@ -6,9 +6,11 @@ import textract
 import tiktoken
 import token_counter
 
+
 KEY = "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"
 MODEL = "gpt-4"
-PROMPT = "./Prompt/" + "Prompt_10:28.txt"
+PROMPT = "./Prompt/" + "dummyPrompt.txt"
+TOWRITE = "./Prompt/" + "output.txt"
 
 llm = ChatOpenAI(openai_api_key=KEY, model_name=MODEL)
 conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
@@ -18,13 +20,25 @@ conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
 allMessages = []
 #Adding the prompt from the .docx file before the conversation begins
 #user_input = textract.process("./Prompt/Chat_GPT_prompt_V1023_OBJ1.docx").decode()
-with open(PROMPT) as prompt:
+with open(PROMPT, "r", encoding="utf8") as prompt:
     user_input = prompt.read()
+
+with open(TOWRITE,"w", encoding = "utf8") as logger: 
+    logger.write(""); 
+
 allMessages.append(user_input)
 
 while user_input != "END":
-    print("GPT: " + conversation.predict(input=user_input) + "\n")
+    output = "GPT: " + conversation.predict(input=user_input) + "\n"
+    print(output)
+
     user_input = input("User: ")
+
+    with open(TOWRITE,"a",encoding="utf8") as logger:
+        logger.write(output)
+        logger.write("User: "+user_input+"\n")
+
     allMessages.append(user_input)
     #Count number of tokens used
     print("no. tokens used: "+str(token_counter.num_tokens_used(allMessages)))
+

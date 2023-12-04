515b7bda0e02fa600470713d487e476bc2162700
ahmedali6395 <ahmedali6395@gmail.com>
2023-12-04 01:23:21 -0500

    time?

---
+++ webapp/tester.py
@@ -0,0 +1,13 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+import time
+import datetime as date
+from docx import Document 
+import io
+import streamlit as st
+
+
+INTRODUCTORY_MESSAGE_LOCATION = '../Prompt/Website_introduction.docx'
+introductory_msg = Document(INTRODUCTORY_MESSAGE_LOCATION)
+print(introductory_msg.paragraphs[1].text)

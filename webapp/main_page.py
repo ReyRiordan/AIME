da76b43043e6f163ebf7e20b769c9cecfc541ada
ReyRiordan <reyriordan@gmail.com>
2023-12-10 23:15:57 -0500

    Beta update (from fork)

--- webapp/main_page.py
+++ webapp/main_page.py
@@ -4,49 +4,43 @@ from langchain.memory import ConversationBufferMemory
 import time
 import datetime as date
 from docx import Document
-import docx as docx
-import os
 import io
+import os
 import streamlit as st
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["LOGIN_PASS"] = st.secrets["LOGIN_PASS"]
+LOGIN_PASS = os.getenv("LOGIN_PASS")
+
+PHYSICAL_LOCATION = "./Patient_Info/Physical_JohnSmith.docx"
+ECG_LOCATION = "./Patient_Info/ECG_JohnSmith.png"
 
 st.title("Medical Interview Simulation")
 
-
-# @REY: Instead of writing down numbers, make each one of those numbers
-#       into its own variable for easy on-the-fly changing. I did the first
-#       one but I'm mad lazy so u got the rest. It's out of order right now, 
-#       but once they're all named, u can then easily swap 'em around. Good
-#       programming practice. 
-
-
-# All page indices
-LOGIN_PAGE = 0
-INTRODUCTORY_INFO_PAGE = 5
-PHYSICAL_EXAMINATION = 6
-
-#All relevant directories
-PROMPT = "./Prompt/Prompt_11-7.txt" #all globals and constants are declared at the start
-INTRODUCTORY_MESSAGE_LOCATION = "./Medical_Info/Website_introduction.docx"
-PHYSICAL_EXAMINATION_LOCATION = "./Medical_Info/Physical_examination_for_first_test_case.docx"
-ECG_LOCATION = "./Medical_Info/ecg.png"
-
-
 if "stage" not in st.session_state:
-    st.session_state["stage"] = LOGIN_PAGE
+    st.session_state["stage"] = 0
 
 def set_stage(stage):
     st.session_state["stage"] = stage
 
 
-if st.session_state["stage"] == LOGIN_PAGE:
-    st.session_state["username"] = st.text_input("Enter a username (does not have to be your real name):")
+if st.session_state["stage"] == 0:
+    st.write("""Welcome! Thank you for agreeing to try out this virtual patient interview platform. Once you log in and get started, 
+             there will be a chat interface where you can interview a virtual patient AI acting as a patient in a specific case study. 
+             The goal is to interview the patient as if it was a real patient in order to work towards a diagnosis. The AI's 
+             conversational responses to your questions may include expressions of worry and we hope that you respond as if it was a real patient.""")
+    st.write("""We are working on developing a feedback rubric that will evaluate the virtual patient interaction with regard to clinical reasoning
+             (e.g. general data gathering, evidence of hypothesis-driven questions, and data interpretation) as well as empathy and clarity. 
+             Once you have completed the interview, we would apprciate any feedback regarding the nature of the responses themselves 
+             (any responses that went off the rails) and their appropriateness to the questions that you asked. We would also appreciate 
+             any feedback on how the interaction may be improved or made more realistic (responses seem too rote, a normal patient wouldn't 
+             use these words, etc). Thank you in advance; please follow the instructions below to log in and get started!""")
+    
+    st.session_state["username"] = st.text_input("Enter any username (does not have to be your real name) and press \"Enter\":")
     if st.session_state["username"]:
-        password = st.text_input("Enter user password:")
-
-        os.environ["PWORD"] = st.secrets["PWORD"]
-        legit_password = os.getenv("PWORD")
-
-        if password == legit_password: 
+        password = st.text_input("Enter the password you were provided and press \"Enter\":")
+        if password == LOGIN_PASS: 
             st.write("Authentication successful!")
             time.sleep(2)
             set_stage(1)
@@ -54,33 +48,28 @@ if st.session_state["stage"] == LOGIN_PAGE:
 
 
 if st.session_state["stage"] == 1:
+    st.write("""This is the patient selection screen. Currently we only have one patient (case study) option, but we hope to expand this further
+             in the future. For now, please just select "John Smith" as your patient and click the "Start Interview" button when you are 
+             ready to begin.""")
+    
     st.session_state["messages"] = []
     st.session_state["patient"] = st.selectbox("Which patient would you like to interview?", 
                                                ["John Smith"],
                                                index = None,
                                                placeholder = "Select patient...")
 
-    st.button("Confirm choice", on_click=set_stage, args=[INTRODUCTORY_INFO_PAGE])
-
-
-if st.session_state["stage"]==INTRODUCTORY_INFO_PAGE: 
-    introductory_msg = Document(INTRODUCTORY_MESSAGE_LOCATION)
-    for parargraph in introductory_msg.paragraphs:
-        st.write(parargraph.text)
-    st.button("Proceed to interview", on_click=set_stage, args=[2])
-
+    st.button("Start Interview", on_click=set_stage, args=[2])
 
 
 if st.session_state["stage"] == 2:
     prompt_input = "default prompt"
     if st.session_state["patient"] == "John Smith":
+        PROMPT = "./Prompt/Prompt_11-7.txt"
         with open(PROMPT, 'r', encoding='utf8') as prompt:
             prompt_input = prompt.read()
 
-    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-    KEY = os.getenv("OPENAI_API_KEY")
     MODEL = "gpt-4"
-    llm = ChatOpenAI(openai_api_key=KEY, model_name=MODEL, temperature=0.7)
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=MODEL, temperature=0.0)
     st.session_state["conversation"] = ConversationChain(llm=llm, memory=ConversationBufferMemory())
     initial_output = st.session_state["conversation"].predict(input=prompt_input)
 
@@ -90,6 +79,12 @@ if st.session_state["stage"] == 2:
 
 
 if st.session_state["stage"] == 3:
+    st.write("""This is the chat interface where you can interview your virtual patient. You may type your message to your patient 
+             in the text box at the bottom of your screen, and either press \"Enter\" or click the paper airplane button when your message 
+             is ready to be sent. The virtual patient should then respond to your message. Please treat this as a real medical interview, 
+             imagining that you have just walked into a room where this patient was waiting. When you are satisfied, click the \"End Interview\" 
+             button to move on to the next screen.""")
+    
     for message in st.session_state["messages"]:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
@@ -103,29 +98,35 @@ if st.session_state["stage"] == 3:
             st.markdown(output)
             st.session_state["messages"].append({"role": st.session_state["patient"], "content": output})
 
-    st.button("End conversation", on_click=set_stage, args=[PHYSICAL_EXAMINATION])
+    st.button("End Interview", on_click=set_stage, args=[4])
 
-if st.session_state["stage"]==PHYSICAL_EXAMINATION:
-    physical_exam_doc = Document(PHYSICAL_EXAMINATION_LOCATION)
-    st.header("Physical Examination Findings")
-    for parargraph in physical_exam_doc.paragraphs:
-        st.write(parargraph.text)
-    st.header("ECG Chart")
-    st.image(ECG_LOCATION)
-    st.button("Proceed to diagnosis and end screen", on_click=set_stage, args=[4])
-    
 
 if st.session_state["stage"] == 4:
-    bio = io.BytesIO()
-    doc = Document()
-    heading = doc.add_paragraph("User: " + st.session_state["username"] + ", ")
+    st.session_state["interview"] = Document()
+    heading = st.session_state["interview"].add_paragraph("User: " + st.session_state["username"] + ", ")
     currentDateAndTime = date.datetime.now()
     date_time = currentDateAndTime.strftime("%d-%m-%y__%H-%M")
     heading.add_run("Date: " + date_time + ", ")
     heading.add_run("Patient: " + st.session_state["patient"])
     for message in st.session_state["messages"]:
-        doc.add_paragraph(message["role"] + ": " + message["content"])
-    doc.save(bio)
+        st.session_state["interview"].add_paragraph(message["role"] + ": " + message["content"])
+    st.session_state["interview"].save("./Conversations/" + st.session_state["username"]+"_"+date_time+".docx")
+    
+    set_stage(5)
+
+if st.session_state["stage"] == 5:
+    st.write("""Thank you so much for completing your interview! A record of the interview has been sent to us. If you would like, you may 
+             view a physical examination and ECG corresponding for the patient in order to get a clearer potential differential diagnosis 
+             in your mind. You may also click the \"Download Interview\" button to save a copy for yourself as a docx file. After receiving 
+             feedback from helpful people like you, we plan to add a screen where you can enter your diagonsis and get feedback on it.""")
+    
+    st.button("View Physical", on_click=set_stage, args=[6])
+    st.button("View ECG", on_click=set_stage, args=[7])
+
+    currentDateAndTime = date.datetime.now()
+    date_time = currentDateAndTime.strftime("%d-%m-%y__%H-%M")
+    bio = io.BytesIO()
+    st.session_state["interview"].save(bio)
     st.download_button("Download interview", 
                         data=bio.getvalue(),
                         file_name=st.session_state["username"]+"_"+date_time+".docx",
@@ -136,3 +137,17 @@ if st.session_state["stage"] == 4:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+if st.session_state["stage"] == 6:
+    st.header("Physical Examination Findings")
+    st.write("Here is the full physical examination for " + st.session_state["patient"] + ". Click the \"Back\" button to go back once you're done.")
+    physical_exam_doc = Document(PHYSICAL_LOCATION)
+    for parargraph in physical_exam_doc.paragraphs:
+        st.write(parargraph.text)
+    st.button("Back", on_click=set_stage, args=[5])
+    
+
+if st.session_state["stage"] == 7:
+    st.header("ECG Chart")
+    st.write("Here is the ECG for " + st.session_state["patient"] + ". Click the \"Back\" button to go back once you're done.")
+    st.image(ECG_LOCATION)
+    st.button("Back", on_click=set_stage, args=[5])

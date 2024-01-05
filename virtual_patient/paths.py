dbc0e068140327933e7202b230f8ca21436735ad
ReyRiordan <reyriordan@gmail.com>
2024-01-04 22:25:37 -0500

    Beginnings of virtual_patient packaging

---
+++ virtual_patient/paths.py
@@ -0,0 +1,6 @@
+BASE_PATH = "./Prompts/Base_12-11.txt"
+
+cases = {
+    "John Smith" : "./Prompts/JohnSmith_12-11.txt",
+    "Jackie Smith" : "./Prompts/JackieSmith_12-11.txt"
+}
---
+++ virtual_patient/patients.py
@@ -0,0 +1,18 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from paths import *
+
+class GPT_patient:
+
+    def __init__(self, name):
+        self.name = name
+
+        with open(BASE_PATH, 'r', encoding='utf8') as base_prompt:
+            base = base_prompt.read()
+        with open(cases[name], 'r', encoding='utf8') as case_prompt:
+            case = case_prompt.read()
+        self.initial_input = str(base + case)
+
+        self.physical_path = "./Patient_Info/Physical_" + self.name.replace(" ", "") + ".docx"
+        self.ECG_path = "./Patient_Info/Physical_" + self.name.replace(" ", "") + ".png"

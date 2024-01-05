d64ccd59d9710d329c6d6177940ff83442de6c84
ReyRiordan <reyriordan@gmail.com>
2024-01-04 22:28:04 -0500

    Quick bug fix

--- webapp/constants.py
+++ webapp/constants.py
@@ -9,17 +9,8 @@ ECG_SCREEN = 6
 FEEDBACK_SCREEN = 7
 FINAL_SCREEN = 8
 
-# FILE LOCATIONS
-PHYSICAL_LOCATION_JOHN = "./Patient_Info/Physical_JohnSmith.docx"
-ECG_LOCATION_JOHN = "./Patient_Info/ECG_JohnSmith.png"
-PHYSICAL_LOCATION_JACKIE = "./Patient_Info/Physical_Jackie.docx"
-ECG_LOCATION_JACKIE="./Patient_Info/ECG_Jackie.jpg"
-
-BASE_PROMPT = "./Prompt/Base_12-11.txt"
-prompts = {
-    "John Smith" : "./Prompt/JohnSmith_12-11.txt",
-    "Jackie Smith" : "./Prompt/JackieSmith_12-11.txt"
-}
+# GPT
+MODEL = "gpt-4"
 
 # EMAIL API
 EMAIL_TO_SEND = [('rutgers.aime@gmail.com')]

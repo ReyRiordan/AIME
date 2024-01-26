5764e0b572a1f9710cdb265ea82738fa4f019dee
ReyRiordan <reyriordan@gmail.com>
2024-01-26 00:57:03 -0500

    langchain community update

--- webapp/main_page.py
+++ webapp/main_page.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 import openai

b0a4731227b6a964412fffb8a1d13d9b42690854
ReyRiordan <reyriordan@gmail.com>
2024-02-25 17:06:56 -0500

    Add diagnosis weights to John Smith grading json

--- webapp/json_transfer.py
+++ webapp/json_transfer.py
@@ -22,43 +22,53 @@ import tempfile
 from annotated_text import annotated_text
 import json
 
-role = {"Personal Details": [{"detail": "Sex", "line": "Female"}, 
-                             {"detail": "Name", "line": "Jackie Smith"}, 
-                             {"detail": "Birthdate", "line": "December 5th 1979"}, 
-                             {"detail": "Personality", "line": "Bright and approachable"}, 
-                             {"detail": "Tone", "line": "Worried and nervous"}, 
-                             {"detail": "Manner of Speech", "line": "Respectful"}], 
-        "Chief Concern": "You came to the hospital because you are experiencing pain in your chest. You are nervous about your symptoms because your father died suddenly of a heart attack when he was 50.", 
-        "History of Present Illness": [{"lock": False, "dim": "Onset", "line": "pain it was very mild at first and has gotten much worse over the day today. You weren’t really doing anything in particular when you first noticed the pain."}, 
-                                       {"lock": False, "dim": "Quality", "line": "sharp, stabbing pain in chest"}, 
-                                       {"lock": False, "dim": "Location", "line": "center of chest"}, 
-                                       {"lock": False, "dim": "Timing", "line": "experiencing chest pain for about a day and half"}, 
-                                       {"lock": False, "dim": "Pattern", "line": "constant"}, 
-                                       {"lock": False, "dim": "Severity", "line": "the pain is very severe"}, 
-                                       {"lock": True, "dim": "Prior History", "line": "you've never had chest pain like you are experiencing now"}, 
-                                       {"lock": True, "dim": "Radiation", "line": "you also notice a pain in along the upper part of your back along both shoulders"}, 
-                                       {"lock": True, "dim": "Exacerbating", "line": "the pain is worse when you breathe in or cough"}, 
-                                       {"lock": True, "dim": "Relieving", "line": "the pain seems to be better when you are sitting up right rather than lying down"}], 
-        "Associated Symptoms": [{"lock": False, "line": "You have felt warm in the past two days. When you took your temperature, it was 100.1 degrees F."}, 
-                                {"lock": False, "line": "You have been feeling very tired and just not well for the past 5 days."}, 
-                                {"lock": False, "line": "You occasionally feel like it can be difficult to breathe (because of the pain), especially when you lay back - it's better if you don't take deep breaths."}], 
-        "Medical History": [{"lock": False, "line": "You had cold symptoms about three weeks ago (they're gone now). You thought it was the flu."}, 
-                            {"lock": True, "line": "You have high cholesterol and take a statin for it."}, 
-                            {"lock": False, "line": "You have been fully boosted for COVID, but do not always get a flu vaccine. Your last COVID booster was one year ago."}], 
-        "Surgical History": [{"lock": False, "line": "N/A"}], 
-        "Medications": [{"lock": False, "line": "If asked about medications, you will say Atorvastatin once a day, but you do not know the dose."}, 
-                        {"lock": True, "line": "If asked about over-the-counter medications, say that you also take Vitamin D and a calcium supplement.  You have also recently started taking a pro-biotic."}], 
-        "Allergies": [{"lock": False, "line": "You are very allergic to bees. Your lips and tongue swelled last time you were stung and you had to go to the Emergency Room- that was 15 years ago."}], 
-        "Family History": [{"lock": False, "line": "Your father died suddenly of a heart attack when he was 50 years old."}, 
-                           {"lock": False, "line": "Your mother was diagnosed with breast cancer two years ago."}], 
-        "Social History": [{"lock": True, "line": "You have never smoked."}, 
-                           {"lock": True, "line": "You drink about 1 glasses of wine or a cocktail during the week."}, 
-                           {"lock": False, "line": "You live with your partner and you have two children together, ages 11 and 8."}, 
-                           {"lock": False, "line": "You are a software engineer."}], 
-        "Other Symptoms": [{"lock": False, "line": "N/A"}]}
+# role = {"Personal Details": [{"detail": "Sex", "line": "Female"}, 
+#                              {"detail": "Name", "line": "Jackie Smith"}, 
+#                              {"detail": "Birthdate", "line": "December 5th 1979"}, 
+#                              {"detail": "Personality", "line": "Bright and approachable"}, 
+#                              {"detail": "Tone", "line": "Worried and nervous"}, 
+#                              {"detail": "Manner of Speech", "line": "Respectful"}], 
+#         "Chief Concern": "You came to the hospital because you are experiencing pain in your chest. You are nervous about your symptoms because your father died suddenly of a heart attack when he was 50.", 
+#         "History of Present Illness": [{"lock": False, "dim": "Onset", "line": "pain it was very mild at first and has gotten much worse over the day today. You weren’t really doing anything in particular when you first noticed the pain."}, 
+#                                        {"lock": False, "dim": "Quality", "line": "sharp, stabbing pain in chest"}, 
+#                                        {"lock": False, "dim": "Location", "line": "center of chest"}, 
+#                                        {"lock": False, "dim": "Timing", "line": "experiencing chest pain for about a day and half"}, 
+#                                        {"lock": False, "dim": "Pattern", "line": "constant"}, 
+#                                        {"lock": False, "dim": "Severity", "line": "the pain is very severe"}, 
+#                                        {"lock": True, "dim": "Prior History", "line": "you've never had chest pain like you are experiencing now"}, 
+#                                        {"lock": True, "dim": "Radiation", "line": "you also notice a pain in along the upper part of your back along both shoulders"}, 
+#                                        {"lock": True, "dim": "Exacerbating", "line": "the pain is worse when you breathe in or cough"}, 
+#                                        {"lock": True, "dim": "Relieving", "line": "the pain seems to be better when you are sitting up right rather than lying down"}], 
+#         "Associated Symptoms": [{"lock": False, "line": "You have felt warm in the past two days. When you took your temperature, it was 100.1 degrees F."}, 
+#                                 {"lock": False, "line": "You have been feeling very tired and just not well for the past 5 days."}, 
+#                                 {"lock": False, "line": "You occasionally feel like it can be difficult to breathe (because of the pain), especially when you lay back - it's better if you don't take deep breaths."}], 
+#         "Medical History": [{"lock": False, "line": "You had cold symptoms about three weeks ago (they're gone now). You thought it was the flu."}, 
+#                             {"lock": True, "line": "You have high cholesterol and take a statin for it."}, 
+#                             {"lock": False, "line": "You have been fully boosted for COVID, but do not always get a flu vaccine. Your last COVID booster was one year ago."}], 
+#         "Surgical History": [{"lock": False, "line": "N/A"}], 
+#         "Medications": [{"lock": False, "line": "If asked about medications, you will say Atorvastatin once a day, but you do not know the dose."}, 
+#                         {"lock": True, "line": "If asked about over-the-counter medications, say that you also take Vitamin D and a calcium supplement.  You have also recently started taking a pro-biotic."}], 
+#         "Allergies": [{"lock": False, "line": "You are very allergic to bees. Your lips and tongue swelled last time you were stung and you had to go to the Emergency Room- that was 15 years ago."}], 
+#         "Family History": [{"lock": False, "line": "Your father died suddenly of a heart attack when he was 50 years old."}, 
+#                            {"lock": False, "line": "Your mother was diagnosed with breast cancer two years ago."}], 
+#         "Social History": [{"lock": True, "line": "You have never smoked."}, 
+#                            {"lock": True, "line": "You drink about 1 glasses of wine or a cocktail during the week."}, 
+#                            {"lock": False, "line": "You live with your partner and you have two children together, ages 11 and 8."}, 
+#                            {"lock": False, "line": "You are a software engineer."}], 
+#         "Other Symptoms": [{"lock": False, "line": "N/A"}]}
 
-# with open("./Patient_Info/JackieSmith_case.json", "w") as json_file:
-#     json.dump(role, json_file, indent=2)
+diagnosis = {"Main": {"Acute coronary syndrome (unstable angina or NSTEMI)": 8}, 
+             "Secondary": {"Acute coronary syndrome (unstable angina or NSTEMI)": 1, 
+                           "Aortic dissection": 1, 
+                           "Acute pericarditis": 1, 
+                           "Gastroesophageal reflux disease (GERD)": 1, 
+                           "Pulmonary embolism": 1, 
+                           "Pneumonia": 1}}
 
-patient = Patient("Jackie Smith")
-print(patient.convo_prompt)
+patient = Patient("John Smith")
+
+grading = {"data": patient.weights, 
+           "diagnosis": diagnosis}
+
+with open("./Patient_Info/JohnSmith_grading.json", "w") as json_file:
+    json.dump(grading, json_file, indent=2)

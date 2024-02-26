d2545dd196b3ef0b9f56f5e4526f446f1660a4e4
ahmedali6395 <ahmedali6395@gmail.com>
2024-02-25 20:49:40 -0500

    comment out stuff needing to be commented oops

--- webapp/main.py
+++ webapp/main.py
@@ -24,9 +24,9 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 
 # SECRETS

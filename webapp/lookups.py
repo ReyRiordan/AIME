c9932b4441d24e137b7c5c53e34a449dd637261d
ReyRiordan <reyriordan@gmail.com>
2024-02-26 14:15:25 -0500

    set convo temp to 0.0

--- webapp/lookups.py
+++ webapp/lookups.py
@@ -16,7 +16,7 @@ VIEW_INTERVIEWS=12
 # LLM related
 CONVO_MODEL = "gpt-4"
 CLASS_MODEL = "gpt-4"
-CHAT_TEMP = 0.7
+CHAT_TEMP = 0.0
 
 SUM_MODEL = "gpt-3.5-turbo-0125"
 with open("./Prompts/Summarizer_2-25.txt", "r", encoding="utf8") as summarizer_file:

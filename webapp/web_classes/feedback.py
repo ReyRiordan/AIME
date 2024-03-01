cce31f5201ace6e1f2f3f7523494a487194a6621
ReyRiordan <reyriordan@gmail.com>
2024-03-01 15:47:11 -0500

    Rename webclasses to web_classes

---
+++ webapp/web_classes/feedback.py
@@ -0,0 +1,19 @@
+from lookups import *
+import json
+from openai import OpenAI
+from patient import *
+from message import *
+from data_acquisition import *
+from diagnosis import *
+            
+class Feedback:
+
+    def __init__(self, patient: Patient, messages: list[Message], userdiagnosis: dict[str, str]):
+        # Attributes
+        self.DataAcquisition = DataAcquisition(patient, messages)
+        self.Diagnosis = Diagnosis(patient, userdiagnosis)
+    
+    def get_dict(self):
+        to_return = {"DataAcquisition": self.DataAcquisition.get_dict(), 
+                     "Diagnosis": self.Diagnosis.get_dict()}
+        return to_return

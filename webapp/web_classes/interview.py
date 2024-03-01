f3970cb8791fa48060a8dc80c5badbf785e2e33d
ReyRiordan <reyriordan@gmail.com>
2024-03-01 18:13:24 -0500

    initialize diagnosis and feedback as None

--- webapp/web_classes/interview.py
+++ webapp/web_classes/interview.py
@@ -12,21 +12,19 @@ class Interview:
 
     def __init__(self, username: str, patient: Patient):
         #Attributes
-        self.__username = username                              # str
-        self.__patient = patient                                # Patient
-        self.__messages = []                                    # list[Message]
-        self.__userdiagnosis = {"main_diagnosis": None,         # dict{str, str}
-                                "main_rationale": None, 
-                                "secondary_diagnosis": None}
-        self.__feedback                                         # Feedback
+        self.__username = username  # str
+        self.__patient = patient    # Patient
+        self.__messages = []        # list[Message]
+        self.__userdiagnosis        # dict{str, str/list[str]}
+        self.__feedback             # Feedback
             
     def add_feedback(self):
         self.__feedback = Feedback(self.__patient, self.__messages, self.__userdiagnosis)
     
-    def add_userdiagnosis(self, main_diagnosis: str, main_rationale: str, secondary_diagnosis: str):
-        self.__userdiagnosis["main_diagnosis"] = main_diagnosis
-        self.__userdiagnosis["main_rationale"] = main_rationale
-        self.__userdiagnosis["secondary_diagnosis"] = secondary_diagnosis
+    def add_userdiagnosis(self, main_diagnosis: str, main_rationale: str, secondary_diagnoses: list[str]):
+        self.__userdiagnosis = {"main_diagnosis": main_diagnosis, 
+                                "main_rationale": main_rationale, 
+                                "secondary_diagnosis": secondary_diagnoses}
     
     def add_message(self, message: Message) -> None:
         if message.type and message.role and message.content:
@@ -53,8 +51,8 @@ class Interview:
                      "username": self.__username, 
                      "patient": self.__patient.get_dict(), 
                      "messages": [message.get_dict() for message in self.__messages], 
-                     "userdiagnosis": self.__userdiagnosis, 
-                     "feedback": self.__feedback.get_dict()}
+                     "userdiagnosis": self.__userdiagnosis if self.__userdiagnosis else None, 
+                     "feedback": self.__feedback.get_dict() if self.__feedback else None}
         return to_return
 
     def get_json(self):

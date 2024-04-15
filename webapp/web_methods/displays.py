2e6a3e3283f95de1cfb7a485f837f7e34b49492f
ReyRiordan <reyriordan@gmail.com>
2024-04-15 14:09:57 -0400

    case explanation tab + download explanation pdf button

--- webapp/web_methods/displays.py
+++ webapp/web_methods/displays.py
@@ -49,14 +49,16 @@ def display_DataAcquisition(data: dict, messages: list[dict]) -> None:
                                  data["scores"][cat_name], 
                                  data["maxscores"][cat_name])
     
-    chat_container = layout1[1].container(height=700)
-    for message in messages:
-            with chat_container:
-                with st.chat_message(message["role"]):
-                    if message["annotation"] is None:
-                        st.markdown(message["content"])
-                    else:
-                        annotated_text((message["content"], message["annotation"], message["highlight"]))
+    with layout1[1]:
+        st.subheader("Annotated Interview Transcript", divider = "grey")
+        chat_container = st.container(height=700)
+        for message in messages:
+                with chat_container:
+                    with st.chat_message(message["role"]):
+                        if message["annotation"] is None:
+                            st.markdown(message["content"])
+                        else:
+                            annotated_text((message["content"], message["annotation"], message["highlight"]))
 
 
 def display_Diagnosis(diagnosis: dict, inputs: dict) -> None:
@@ -66,50 +68,62 @@ def display_Diagnosis(diagnosis: dict, inputs: dict) -> None:
     checklists = diagnosis["checklists"]
     weights = diagnosis["weights"]
 
-    layout1 = st.columns([1, 1])
-
-    with layout1[0].container(border = True):
+    with st.container(border = True):
         st.subheader(f"Interpretative Summary: {scores['Summary']}/{maxscores['Summary']}", divider = "grey")
-        st.write(inputs["Summary"])
-        display_labels = [(key, str(weights["Summary"][key]), "#baffc9" if value else "#ffb3ba") for key, value in checklists["Summary"].items()]
-        annotated_text(display_labels)
-        with st.expander("Label Descriptions:"):
-            for key, value in checklists["Summary"].items():
-                annotated_text([(key, "", "#baffc9" if value else "#ffb3ba"), " " + LABEL_DESCS[key]])
+        layout1 = st.columns([1, 1])
+        with layout1[0]:
+            st.write(inputs["Summary"])
+        with layout1[1]:
+            display_labels = [(key, str(weights["Summary"][key]), "#baffc9" if value else "#ffb3ba") for key, value in checklists["Summary"].items()]
+            annotated_text(display_labels)
+            with st.expander("Label Descriptions:"):
+                for key, value in checklists["Summary"].items():
+                    annotated_text([(key, "", "#baffc9" if value else "#ffb3ba"), " " + LABEL_DESCS[key]])
 
-    with layout1[0].container(border = True):
+    with st.container(border = True):
         st.subheader(f"Potential Diagnoses: {scores['Potential']}/{maxscores['Potential']}", divider = "grey")
-        user_potential = [(key, value, "#baffc9" if value in checklists["Potential"] else "#ffb3ba") for key, value in classified["Potential"].items()]
-        annotated_text(["Your answers: "] + user_potential)
-        valid_potential = [(key, str(weights["Potential"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Potential"].items()]
-        annotated_text(["Valid answers: "] + valid_potential)
+        layout2 = st.columns([1, 1])
+        with layout2[0]:
+            user_potential = [(key, value, "#baffc9" if value in checklists["Potential"] else "#ffb3ba") for key, value in classified["Potential"].items()]
+            annotated_text(["Your answers: "] + user_potential)
+        with layout2[1]:
+            valid_potential = [(key, str(weights["Potential"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Potential"].items()]
+            annotated_text(["Valid answers: "] + valid_potential)
         
-    with layout1[0].container(border = True):
+    with st.container(border = True):
         st.subheader(f"Rationale: {scores['Rationale']}/{maxscores['Rationale']}", divider = "grey")
-        st.write(inputs["Rationale"])
-        for key, value in checklists["Rationale"].items():
-            annotated_text((key, str(weights["Rationale"][key]), "#baffc9" if value else "#ffb3ba"))
+        layout3 = st.columns([1, 1])
+        with layout3[0]:
+            st.write(inputs["Rationale"])
+        with layout3[1]:
+            for key, value in checklists["Rationale"].items():
+                annotated_text((key, str(weights["Rationale"][key]), "#baffc9" if value else "#ffb3ba"))
 
-    with layout1[0].container(border = True):
+    with st.container(border = True):
         st.subheader(f"Final Diagnosis: {scores['Final']}/{maxscores['Final']}", divider = "grey")
-        user_final = [(key, value, "#baffc9" if value in checklists["Final"] else "#ffb3ba") for key, value in classified["Final"].items()]
-        annotated_text("Your answer: ", user_final)
-        valid_final = [(key, str(weights["Final"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Final"].items()]
-        annotated_text("Valid answer(s): ", valid_final)
-
-    with layout1[1].container():
-        st.write("Insert explanations, examples, or additional feedback here...")
+        layout4 = st.columns([1, 1])
+        with layout4[0]:
+            user_final = [(key, value, "#baffc9" if value in checklists["Final"] else "#ffb3ba") for key, value in classified["Final"].items()]
+            annotated_text("Your answer: ", user_final)
+        with layout4[1]:
+            valid_final = [(key, str(weights["Final"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Final"].items()]
+            annotated_text("Valid answer(s): ", valid_final)
 
 
 def display_Interview(interview: dict) -> None:
     st.write(f"{interview['username']} @ {interview['date_time']}, Patient: {interview['patient']['name']}")
 
     if interview["feedback"]:
-        data, diagnosis, empathy = st.tabs(["Data Acquisition", "Diagnosis", "Empathy"])
+        data, diagnosis, explanation = st.tabs(["Data Acquisition", "Diagnosis", "Case Explanation"])
         with data:
             display_DataAcquisition(interview["feedback"]["Data Acquisition"], interview["messages"])
         with diagnosis:
             display_Diagnosis(interview["feedback"]["Diagnosis"], interview["diagnosis_inputs"])
+        with explanation:
+            explanation_file = st.session_state["interview"].get_patient().explanation
+            with open(explanation_file, "rb") as pdf_file:
+                explanation = pdf_file.read()
+                st.download_button("Download Case Explanation (PDF)", explanation, explanation_file)
     else:
         chat_container = st.container(height=300)
         for message in st.session_state["interview"]["messages"]:

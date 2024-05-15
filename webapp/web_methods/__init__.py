6a20b8d81c4f091f05b83e8de69b9ed894fb2193
ReyRiordan <reyriordan@gmail.com>
2024-05-15 16:08:20 -0400

    remove get_webtext from init

--- webapp/web_methods/__init__.py
+++ webapp/web_methods/__init__.py
@@ -2,7 +2,7 @@ from .displays import *
 from .files import *
 from .LLM import *
 
-__all__ = ["get_webtext", "display_DataCategory", "display_DataAcquisition", "display_Diagnosis", "display_Interview", 
+__all__ = ["display_DataCategory", "display_DataAcquisition", "display_Diagnosis", "display_Interview", 
            "create_convo_file", "send_email", 
            "generate_response", "generate_classifications", "generate_matches", 
            "transcribe_voice", "generate_voice", "play_voice", 

cb18a816f23fbffa9d0bcd96f0f27f302cb0cf98
ahmedali6395 <ahmedali6395@gmail.com>
2024-05-30 15:05:57 -0400

    commenting out stuff so it works

--- webapp/web_methods/displays.py
+++ webapp/web_methods/displays.py
@@ -166,121 +166,121 @@ def display_Interview(interview: dict) -> None:
 
 #TODO: NOT EVEN CLOSE TO DONE, PROBLEM FOR @ALI
 
-def display_Interview_NEW(interview:Interview)->None:
-    st.write(f"{interview.username} @ {interview.start_time}, Patient {interview.patient.name}")
+# def display_Interview_NEW(interview:Interview)->None:
+#     st.write(f"{interview.username} @ {interview.start_time}, Patient {interview.patient.name}")
 
-    if interview.feedback:
-        data, diagnosis, empathy = st.tabs(["Data Acquisition", "Diagnosis", "Empathy"])
-        with data:
-            display_DataAcquisition_NEW(interview.feedback.data_acquisition, interview.messages)
-        with diagnosis:
-            display_Diagnosis_NEW(interview.feedback.diagnosis, interview.diagnosis_inputs)
-    else:
-        chat_container = st.container(height=300)
-        for message in interview.messages:
-            with chat_container:
-                with st.chat_message(message.role):
-                    st.markdown(message.content)
+#     if interview.feedback:
+#         data, diagnosis, empathy = st.tabs(["Data Acquisition", "Diagnosis", "Empathy"])
+#         with data:
+#             display_DataAcquisition_NEW(interview.feedback.data_acquisition, interview.messages)
+#         with diagnosis:
+#             display_Diagnosis_NEW(interview.feedback.diagnosis, interview.diagnosis_inputs)
+#     else:
+#         chat_container = st.container(height=300)
+#         for message in interview.messages:
+#             with chat_container:
+#                 with st.chat_message(message.role):
+#                     st.markdown(message.content)
                     
-        if interview.diagnosis_inputs:
-            diagnosis_inputs = interview.diagnosis_inputs
-            st.divider()
-            st.write("Interpretative Summary: " + diagnosis_inputs["Summary"])
-            st.write("Main Diagnosis: " + diagnosis_inputs["Potential"])
-            st.write("Main Rationale: " + diagnosis_inputs["Rationale"])
-            st.write("Secondary Diagnoses: " + ", ".join(diagnosis_inputs["Final"]))
+#         if interview.diagnosis_inputs:
+#             diagnosis_inputs = interview.diagnosis_inputs
+#             st.divider()
+#             st.write("Interpretative Summary: " + diagnosis_inputs["Summary"])
+#             st.write("Main Diagnosis: " + diagnosis_inputs["Potential"])
+#             st.write("Main Rationale: " + diagnosis_inputs["Rationale"])
+#             st.write("Secondary Diagnoses: " + ", ".join(diagnosis_inputs["Final"]))
 
-def display_DataAcquisition_NEW(data:DataAcquisition, messages:List[Message])->None:
-    layout1 = st.columns([4, 5])
-    with layout1[0]:
-        for category in data.datacategories:
-            cat_name = category.name
-            display_DataCategory_NEW(category, 
-                                 data.checklists[cat_name], 
-                                 data.weights[cat_name], 
-                                 data.scores[cat_name], 
-                                 data.maxscores[cat_name])
+# def display_DataAcquisition_NEW(data:DataAcquisition, messages:List[Message])->None:
+#     layout1 = st.columns([4, 5])
+#     with layout1[0]:
+#         for category in data.datacategories:
+#             cat_name = category.name
+#             display_DataCategory_NEW(category, 
+#                                  data.checklists[cat_name], 
+#                                  data.weights[cat_name], 
+#                                  data.scores[cat_name], 
+#                                  data.maxscores[cat_name])
     
-    chat_container = layout1[1].container(height=500)
-    for message in messages:
-            with chat_container:
-                with st.chat_message(message.role):
-                    if message.annotation is None:
-                        st.markdown(message.content)
-                    else:
-                        annotated_text((message.content, message.annotation, message.highlight))
+#     chat_container = layout1[1].container(height=500)
+#     for message in messages:
+#             with chat_container:
+#                 with st.chat_message(message.role):
+#                     if message.annotation is None:
+#                         st.markdown(message.content)
+#                     else:
+#                         annotated_text((message.content, message.annotation, message.highlight))
 
 
-def display_Diagnosis_NEW(diagnosis: Diagnosis, inputs: dict) -> None:
-    scores = diagnosis.scores
-    maxscores = diagnosis.maxscores
-    classified = diagnosis.classified
-    checklists = diagnosis.checklists
-    weights = diagnosis.weights
+# def display_Diagnosis_NEW(diagnosis: Diagnosis, inputs: dict) -> None:
+#     scores = diagnosis.scores
+#     maxscores = diagnosis.maxscores
+#     classified = diagnosis.classified
+#     checklists = diagnosis.checklists
+#     weights = diagnosis.weights
 
-    with st.container(border = True):
-        st.subheader(f"Interpretative Summary: {scores['Summary']}/{maxscores['Summary']}", divider = "grey")
-        layout1 = st.columns([1, 1])
-        with layout1[0]:
-            st.write("**Your answer:**")
-            st.write(inputs["Summary"])
-        with layout1[1]:
-            st.write("**Scoring:**")
-            display_labels = [(key, str(weights["Summary"][key]), "#baffc9" if value else "#ffb3ba") for key, value in checklists["Summary"].items()]
-            annotated_text(display_labels)
-            with st.expander("**Label Descriptions:**"):
-                for key, value in checklists["Summary"].items():
-                    annotated_text([(key, "", "#baffc9" if value else "#ffb3ba"), " " + LABEL_DESCS[key]])
+#     with st.container(border = True):
+#         st.subheader(f"Interpretative Summary: {scores['Summary']}/{maxscores['Summary']}", divider = "grey")
+#         layout1 = st.columns([1, 1])
+#         with layout1[0]:
+#             st.write("**Your answer:**")
+#             st.write(inputs["Summary"])
+#         with layout1[1]:
+#             st.write("**Scoring:**")
+#             display_labels = [(key, str(weights["Summary"][key]), "#baffc9" if value else "#ffb3ba") for key, value in checklists["Summary"].items()]
+#             annotated_text(display_labels)
+#             with st.expander("**Label Descriptions:**"):
+#                 for key, value in checklists["Summary"].items():
+#                     annotated_text([(key, "", "#baffc9" if value else "#ffb3ba"), " " + LABEL_DESCS[key]])
 
-    with st.container(border = True):
-        st.subheader(f"Potential Diagnoses: {scores['Potential']}/{maxscores['Potential']}", divider = "grey")
-        layout2 = st.columns([1, 1])
-        with layout2[0]:
-            st.write("**Your answer(s):**")
-            user_potential = [(key, value, "#baffc9" if value in checklists["Potential"] else "#ffb3ba") for key, value in classified["Potential"].items()]
-            annotated_text(user_potential)
-        with layout2[1]:
-            st.write("**Valid answers:**")
-            valid_potential = [(key, str(weights["Potential"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Potential"].items()]
-            annotated_text(valid_potential)
+#     with st.container(border = True):
+#         st.subheader(f"Potential Diagnoses: {scores['Potential']}/{maxscores['Potential']}", divider = "grey")
+#         layout2 = st.columns([1, 1])
+#         with layout2[0]:
+#             st.write("**Your answer(s):**")
+#             user_potential = [(key, value, "#baffc9" if value in checklists["Potential"] else "#ffb3ba") for key, value in classified["Potential"].items()]
+#             annotated_text(user_potential)
+#         with layout2[1]:
+#             st.write("**Valid answers:**")
+#             valid_potential = [(key, str(weights["Potential"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Potential"].items()]
+#             annotated_text(valid_potential)
         
-    with st.container(border = True):
-        st.subheader(f"Rationale: {scores['Rationale']}/{maxscores['Rationale']}", divider = "grey")
-        layout3 = st.columns([1, 1])
-        with layout3[0]:
-            st.write("**Your answer:**")
-            st.write(inputs["Rationale"])
-        with layout3[1]:
-            st.write("**Scoring:**")
-            for condition in weights["Rationale"]:
-                if condition in checklists["Rationale"]:
-                    with st.expander(f"**{condition}:**"):
-                        for statement, checked in checklists["Rationale"][condition].items():
-                            annotated_text((statement, str(weights["Rationale"][condition][statement]), "#baffc9" if checked else "#ffb3ba"))
-            with st.expander("**Reasoning for potential diagnoses you didn't list:**"):
-                for condition in weights["Rationale"]:
-                    if condition not in checklists["Rationale"]:
-                        for statement, weight in weights["Rationale"][condition].items():
-                            annotated_text((statement, str(weight), "#ededed"))
+#     with st.container(border = True):
+#         st.subheader(f"Rationale: {scores['Rationale']}/{maxscores['Rationale']}", divider = "grey")
+#         layout3 = st.columns([1, 1])
+#         with layout3[0]:
+#             st.write("**Your answer:**")
+#             st.write(inputs["Rationale"])
+#         with layout3[1]:
+#             st.write("**Scoring:**")
+#             for condition in weights["Rationale"]:
+#                 if condition in checklists["Rationale"]:
+#                     with st.expander(f"**{condition}:**"):
+#                         for statement, checked in checklists["Rationale"][condition].items():
+#                             annotated_text((statement, str(weights["Rationale"][condition][statement]), "#baffc9" if checked else "#ffb3ba"))
+#             with st.expander("**Reasoning for potential diagnoses you didn't list:**"):
+#                 for condition in weights["Rationale"]:
+#                     if condition not in checklists["Rationale"]:
+#                         for statement, weight in weights["Rationale"][condition].items():
+#                             annotated_text((statement, str(weight), "#ededed"))
 
-    with st.container(border = True):
-        st.subheader(f"Final Diagnosis: {scores['Final']}/{maxscores['Final']}", divider = "grey")
-        layout4 = st.columns([1, 1])
-        with layout4[0]:
-            st.write("**Your answer:**")
-            user_final = [(key, value, "#baffc9" if value in checklists["Final"] else "#ffb3ba") for key, value in classified["Final"].items()]
-            annotated_text("Your answer: ", user_final)
-        with layout4[1]:
-            st.write("**Valid answer(s):**")
-            valid_final = [(key, str(weights["Final"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Final"].items()]
-            annotated_text(valid_final)
+#     with st.container(border = True):
+#         st.subheader(f"Final Diagnosis: {scores['Final']}/{maxscores['Final']}", divider = "grey")
+#         layout4 = st.columns([1, 1])
+#         with layout4[0]:
+#             st.write("**Your answer:**")
+#             user_final = [(key, value, "#baffc9" if value in checklists["Final"] else "#ffb3ba") for key, value in classified["Final"].items()]
+#             annotated_text("Your answer: ", user_final)
+#         with layout4[1]:
+#             st.write("**Valid answer(s):**")
+#             valid_final = [(key, str(weights["Final"][key]), "#baffc9" if value else "#ffb3ba") for key, value, in checklists["Final"].items()]
+#             annotated_text(valid_final)
 
 
-def display_DataCategory_NEW(category: DataCategory, checklist: dict[str, bool], weights: dict[str, int], score: int, maxscore: int) -> None:
-    with st.container(border = True):
-        st.subheader(f":{category.color}[{category.header}]: {score}/{maxscore}", divider = category['color'])
-        display_labels = [(key, str(weights[key]), "#baffc9" if value else "#ffb3ba") for key, value in checklist.items()]
-        annotated_text(display_labels)
-        with st.expander("Label Descriptions:"):
-            for key, value in checklist.items():
-                annotated_text([(key, "", "#baffc9" if value else "#ffb3ba"), " " + LABEL_DESCS[key]])
+# def display_DataCategory_NEW(category: DataCategory, checklist: dict[str, bool], weights: dict[str, int], score: int, maxscore: int) -> None:
+#     with st.container(border = True):
+#         st.subheader(f":{category.color}[{category.header}]: {score}/{maxscore}", divider = category['color'])
+#         display_labels = [(key, str(weights[key]), "#baffc9" if value else "#ffb3ba") for key, value in checklist.items()]
+#         annotated_text(display_labels)
+#         with st.expander("Label Descriptions:"):
+#             for key, value in checklist.items():
+#                 annotated_text([(key, "", "#baffc9" if value else "#ffb3ba"), " " + LABEL_DESCS[key]])

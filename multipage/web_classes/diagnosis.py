775033fef9a5016ec404d3f914c58c09aab2c133
ReyRiordan <reyriordan@gmail.com>
2024-06-30 19:53:14 -0400

    simplify/revamp entire structure of diagnosis grading (will do scoring later)

--- multipage/web_classes/diagnosis.py
+++ multipage/web_classes/diagnosis.py
@@ -10,88 +10,82 @@ from .message import *
 import web_methods
 
 class Diagnosis(pydantic.BaseModel):
-    grading: dict
-    classified : Optional[dict]
-    checklists: Optional[dict]
+    matches : Optional[dict]
+    grades: Optional[dict]
     scores : Optional[dict]
     maxscores : Optional[dict] 
 
 
     # Attributes
-        # grading = patient.grading["Diagnosis"]  # dict{str, dict{str, int}}
-        # classified = None                       # dict{str, dict{str, str}}
+        # matches = None                       # dict{str, dict{str, str}}
         # checklists = None                       # dict{str, dict{str, bool}}
         # score = None                            # dict{int}
         # maxscore = None                         # dict{int}
     
     @classmethod
     def build(cls, patient: Patient, inputs: dict[str, str]):
-        grading = patient.grading["Diagnosis"]
                 
-        # Intialize the checklists other than rationale
-        checklists = {"Summary": {}, 
-                      "Rationale": {}, 
-                      "Potential": {}, 
-                      "Final": {}}
-        for label in grading["Summary"]:
-            checklists["Summary"][label] = False
-        for condition in grading["Potential"]:
-            checklists["Potential"][condition] = False
-        for condition in grading["Final"]:
-            checklists["Final"][condition] = False
+        # Intialize big grades dict
+        patient_grading = patient.grading["Diagnosis"]
+        grades = {"Summary": {}, # label: {weight, score}
+                  "Rationale": {}, # condition: {weight, score}
+                  "Potential": {}, # condition: [{desc, sign, weight, score}, etc.]
+                  "Final": {}} # condition: {weight, score}
+        for label, weight in patient_grading["Summary"].items():
+            grades["Summary"][label] = {"weight": weight, "score": False}
+        for condition, weight in patient_grading["Potential"].items():
+            grades["Potential"][condition] = {"weight": weight, "score": False}
+        for condition in patient_grading["Rationale"]:
+            grades["Rationale"][condition] = []
+            for reasoning in patient_grading["Rationale"][condition]:
+                to_append = reasoning.copy()
+                to_append["score"] = False
+                grades["Rationale"][condition].append(to_append)
+        for condition, weight in patient_grading["Final"].items():
+            grades["Final"][condition] = {"weight": weight, "score": False}
+        
+        # Initialize dict to see user input and corresponding matched conditions
+        matches = {"Potential": {}, 
+                   "Final": {}}
         
         # Grade the summary
         sum_prompt = GRADE_SUM_PROMPT
-        for label in grading["Summary"]:
+        for label in grades["Summary"]:
             sum_prompt += f"[{label}]\n{LABEL_DESCS[label]}\n"
         output = web_methods.generate_classifications(sum_prompt, inputs["Summary"])
         print(output + "\n\n")
         sum_labels = json.loads(output)
         for label in sum_labels:
-            if label in checklists["Summary"]:
-                checklists["Summary"][label] = True
-
-        # Dict to see user input and corresponding matched conditions
-        classified = {"Potential": {}, 
-                      "Final": {}}
+            if label in grades["Summary"]:
+                grades["Summary"][label]["score"] = True
         
-        # Grade the conditions
+        # Grade the potential and final conditions
         main_prompt = GRADE_DIAG_PROMPT
         valid_conditions = []
-        for condition in checklists["Potential"]:
+        for condition in grades["Potential"]:
             if condition not in valid_conditions:
                 valid_conditions.append(condition)
-        for condition in checklists["Final"]:
+        for condition in grades["Final"]:
             if condition not in valid_conditions:
                 valid_conditions.append(condition)
         main_prompt += json.dumps(valid_conditions)
         user_inputs = [diagnosis for diagnosis in inputs["Potential"]] + [inputs["Final"]]
         # print(f"User inputs: {user_inputs}\n")
         output = web_methods.generate_matches(main_prompt, json.dumps(user_inputs))
-        print(output + "\n\n")
-        matches = json.loads(output)
-        print(f"Matches: {matches}\n\n")
-
+        print(output + "\n\n") # debugging
+        input_matches = json.loads(output)
+        print(f"Matches: {input_matches}\n\n") # debugging
         for diagnosis in inputs["Potential"]:
-            classified["Potential"][diagnosis] = matches[diagnosis]
-            if matches[diagnosis] in checklists["Potential"]:
-                checklists["Potential"][matches[diagnosis]] = True
-        classified["Final"][inputs["Final"]] = matches[inputs["Final"]]
-        if matches[inputs["Final"]] in checklists["Final"]:
-            checklists["Final"][matches[inputs["Final"]]] = True
-
-        # Initialize rationale checklist
-        for condition in grading["Rationale"]:
-            if checklists["Potential"][condition]:
-                checklists["Rationale"][condition] = {} # {condition: {id: True/False}
-                id = 1
-                for reasoning in grading["Rationale"][condition]:
-                    checklists["Rationale"][condition][id] = False
-                    id += 1
+            matches["Potential"][diagnosis] = input_matches[diagnosis]
+            if input_matches[diagnosis] in grades["Potential"]:
+                grades["Potential"][input_matches[diagnosis]]["score"] = True
+        matches["Final"][inputs["Final"]] = input_matches[inputs["Final"]]
+        if input_matches[inputs["Final"]] in grades["Final"]:
+            grades["Final"][input_matches[inputs["Final"]]]["score"] = True
         
         # Grade the rationale
         rat_prompt = GRADE_RAT_PROMPT
-        for condition, reasonings in grading["Rationale"].items():
+        for condition, reasonings in grades["Rationale"].items():
             reasonings_dict = {} # {id: "IMPLIES/REFUTES: desc"}
             id = 1
             for reasoning in reasonings:
@@ -99,15 +93,13 @@ class Diagnosis(pydantic.BaseModel):
                 reasonings_dict[id] = sign + reasoning["desc"]
                 id += 1
             rat_prompt += f"\"{condition}\" {reasonings_dict}\n"
-        print(rat_prompt + "\n\n")
+        print(rat_prompt + "\n\n") # debugging
         output = web_methods.generate_classifications(rat_prompt, inputs["Rationale"])
-        print(output + "\n\n")
+        print(output + "\n\n") # debugging
         rat_grades = json.loads(output) # {condition: [id, id, etc.]} (only ids that were present)
         for condition in rat_grades:
             for id in rat_grades[condition]:
-                checklists["Rationale"][condition][id] = True
-
-        # print(checklists)
+                grades["Rationale"][condition][id-1]["score"] = True # prompted ids are 1 start but indices are 0 start
         
         # Calculate scoring
         scores = {"Summary": 0,
@@ -134,7 +126,7 @@ class Diagnosis(pydantic.BaseModel):
             if checklists["Final"][condition]:
                 scores["Final"] += grading["Final"][condition]
 
-        return cls(grading=grading, classified=classified, checklists=checklists, scores=scores, maxscores=maxscores)
+        return cls(matches=matches, grades=grades, scores=scores, maxscores=maxscores)
 
     # def get_dict(self):
     #     to_return = {"grading": self.grading, 

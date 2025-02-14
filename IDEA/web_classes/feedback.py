86d55bc8390da1d4c8ce5d60f3bfc8eceabbe635
ReyRiordan <reyriordan@gmail.com>
2025-02-14 02:32:43 -0500

    text feedback building first draft, just need scores

--- IDEA/web_classes/feedback.py
+++ IDEA/web_classes/feedback.py
@@ -6,22 +6,54 @@ from typing import Optional, List
 
 from .patient import *
 from .message import *
-from .data_acquisition import *
-from .diagnosis import *
+from web_methods.LLM import *
             
 class Feedback(pydantic.BaseModel):
 
-    data_acquisition: Optional[DataAcquisition] = None  #DataAcquisition
-    diagnosis: Optional[Diagnosis]             = None  #Diagnosis
+    feedback: Optional[dict] = {}           # {type: {text: str, points: int}}
 
     @classmethod
-    def build(cls, patient: Patient, messages: list[Message], diagnosis_inputs: dict[str, str]):
-        # Attributes
-        to_return_data_acquisition = DataAcquisition.build(patient=patient, messages=messages)
-        return cls(data_acquisition=to_return_data_acquisition, diagnosis=Diagnosis.build(patient=patient, inputs=diagnosis_inputs))
+    def build(cls, patient: Patient, messages: list[Message], post_note: dict[str, str]):
+
+        # Import rubric bases
+        with open("./Prompts/base_rubrics.json", "r", ) as json_file:
+            BASE = json.load(json_file)
+
+        # Initialize
+        feedback = {"Key Findings": None,
+                    "HPI": [], # 3
+                    "Past Histories": [], # 2
+                    "Summary": None,
+                    "Assessment": [], # 3
+                    "Plan": None}
+
+        # Feedback and grading
+        for category in ["Key Findings", "HPI", "Past Histories", "Summary", "Assessment", "Plan"]:
+            if feedback[category] is not None:
+                for p, d in BASE[category].items():
+                    response = generate_feedback(title = d["title"],
+                                                 desc = d["desc"],
+                                                 rubric = patient.grading[category][p]["rubric"],
+                                                 user_input = post_note[category])
+                    # score = generate_score()
+                    feedback[category].append({"text": response,
+                                            #    "score": score,
+                                               "max": patient.grading[category][p]["points"]})
+            else:
+                response = generate_feedback(title = BASE[category]["title"],
+                                             desc = BASE[category]["desc"],
+                                             rubric = patient.grading[category]["rubric"],
+                                             user_input = post_note[category])
+                # score = generate_score()
+                feedback[category].append({"text": response,
+                                        #    "score": score,
+                                           "max": patient.grading[category]["points"]})
+
+        return cls(feedback=feedback)
 
         # self.data_acquisition = DataAcquisition(patient, messages)
-        # self.diagnosis = Diagnosis(patient, user_diagnosis)
+        # self.diagnosis = Diagnosis(patient, user_diagnosis)        
+    
     
 
     

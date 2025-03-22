bc703be35c12d966375068da4b983ade1e091a1e
ReyRiordan <reyriordan@gmail.com>
2025-03-22 00:09:27 -0400

    M2 assignments cleaned

--- IDEA/assignments/misc.py
+++ IDEA/assignments/misc.py
@@ -2,29 +2,49 @@ import pandas as pd
 import json
 import math
 
-# Load the Excel file with all sheets
+# Load the Excel file
 xls = pd.ExcelFile("assignments_original.xlsx")
 
-# Convert each sheet to a dictionary of lists
+# Convert each sheet to a dict
 sheets_dict = {sheet: xls.parse(sheet).to_dict(orient="records") for sheet in xls.sheet_names}
 M1_original = sheets_dict["M1's Randomized Cases"]
+M2_original = sheets_dict["M2's Randomized Cases"]
 
-M1 = []
-required_keys = ["LAST_NAME", "FIRST_NAME", "NetID", "First Case (Atypical 1 - Jenny or Jeffrey)", "Second Case (Atypical 2 - Sam or Sarah)"]
-for student in M1_original:
-    if all(key in student and not (isinstance(student[key], float) and math.isnan(student[key])) for key in required_keys):
-        M1.append({"Last_name": student["LAST_NAME"], 
-                   "First_name": student["FIRST_NAME"], 
-                   "NetID": student["NetID"], 
-                   "First_case": student["First Case (Atypical 1 - Jenny or Jeffrey)"], 
-                   "Second_case": student["Second Case (Atypical 2 - Sam or Sarah)"]})
+# Methods for cleaning
+def valid_student(student, required_keys):
+    return all(key in student and not (isinstance(student[key], float) and math.isnan(student[key])) for key in required_keys)
 
+def clean(original, required_keys):
+    cleaned = []
+    for student in original:
+        if valid_student(student, required_keys):
+            cleaned.append({"Last_name": student["LAST_NAME"], 
+                    "First_name": student["FIRST_NAME"], 
+                    "NetID": student["NetID"], 
+                    "First_case": student[required_keys[3]], 
+                    "Second_case": student[required_keys[4]]})
+    return cleaned
 
-# Save to a properly formatted JSON file
+# Clean
+required_M1 = ["LAST_NAME", "FIRST_NAME", "NetID", "First Case (Atypical 1 - Jenny or Jeffrey)", "Second Case (Atypical 2 - Sam or Sarah)"]
+M1 = clean(M1_original, required_M1)
+required_M2 = ["LAST_NAME", "FIRST_NAME", "NetID", "First Case (Atypica 1l)", "Second Case (Atypical 2)"]
+M2 = clean(M2_original, required_M2)
+
+# Check number of students
+print(len(M1))
+print(len(M2))
+
+# Save to JSONs
 with open("M1_original.json", "w") as json_file:
+    json.dump(M1_original, json_file, indent=2)
+with open("M2_original.json", "w") as json_file:
     json.dump(M1_original, json_file, indent=2)
 
 with open("M1.json", "w") as json_file:
     json.dump(M1, json_file, indent=2)
+with open("M2.json", "w") as json_file:
+    json.dump(M1, json_file, indent=2)
 
+# Exit
 print("Conversion complete!")

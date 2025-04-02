569dcf244d8e9560bc4dd2b43f03b49cb4da429f
ReyRiordan <reyriordan@gmail.com>
2025-04-01 20:21:49 -0400

    new msg

--- IDEA/short.py
+++ IDEA/short.py
@@ -61,8 +61,7 @@ if st.session_state["stage"] == LOGIN_PAGE:
     layout1 = st.columns([2, 3, 2])
     with layout1[1]:
         st.title("Medical Interview Simulation")
-        st.write("**WARNING: The AI company that supplies the base model for our feedback is currently experiencing an outage. You are still able to do interviews and write your post notes, however you will likely not be able to get your feedback. Clicking the \"Get Feedback\" button will still save all of your responses and count as participation, but we recommend waiting till the outage is over.**")
-        st.write("**Due to this issue, the deadline has been extended to Wednesday (tomorrow) at midnight and will be extended further if necessary. So sorry for the inconvenience!**")
+        st.write("**NOTICE: We are aware of a brief outage that occurred with our feedback AI - the issue has already been resolved. All students that encountered this error do not have to redo their case as all post note responses and interviews were saved automatically. The deadline has been extended 24 hours to Wednesday at midnight for now. We apologize for the inconvenience!**")
         st.write("Welcome! This is a WIP application where you can interview AI patients, write a post note, and automatically receive feedback on your performance.")
         st.write("Begin by logging in as directed. If you encounter any issues, please contact rhr58@scarletmail.rutgers.edu")
 

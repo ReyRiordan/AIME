8781f282f7bdb061170b23fe26d5c3697af51dfa
ReyRiordan <reyriordan@gmail.com>
2025-06-22 23:47:13 -0400

    no default startup case (can manually select), improved time tracking

--- IDEA/human_eval.py
+++ IDEA/human_eval.py
@@ -35,10 +35,6 @@ if "stage" not in st.session_state:
 def set_stage(stage):
     st.session_state["stage"] = stage
 
-def record_time(evaluation):
-    current_time = datetime.now().isoformat()
-    evaluation["times"].append(current_time)
-
 
 # DB SETUP
 @st.cache_resource
@@ -49,14 +45,48 @@ DB_CLIENT = init_connection()
 COLLECTION_INTERVIEWS = DB_CLIENT["Benchmark"]["Interviews"]["M2_test"]
 COLLECTION_EVALUATIONS = DB_CLIENT["Benchmark"]["Human_Eval"]["M2_test"]
 
-def update_evaluation(evaluation):
+def update_evaluation(checkpoint: str, evaluation: dict):
     """Update or insert an evaluation document in the database."""
-    record_time(evaluation)
+    current_time = datetime.now().isoformat()
+    evaluation['times'][current_time] = checkpoint
     query = {
         "username": st.session_state["username"],
         "interview_info._id": evaluation["interview_info"]["_id"]
     }
     COLLECTION_EVALUATIONS.replace_one(query, evaluation, upsert=True)
+
+# OTHER
+def load_and_setup():
+    # Load the current interview based on view_index
+    interview_id = st.session_state["interview_list"][st.session_state["current_index"]]["_id"]
+    interview = COLLECTION_INTERVIEWS.find_one({"_id": interview_id})
+
+    # Load or create the evaluation for this interview
+    evaluation = COLLECTION_EVALUATIONS.find_one({"username": st.session_state["username"], "interview_info._id": interview_id})
+    if not evaluation:
+        evaluation = {
+            "username": st.session_state["username"],
+            "interview_info": st.session_state["interview_list"][st.session_state["current_index"]],
+            "mark": "",
+            "times": {}
+        }
+        # Initialize feedback structure
+        feedback = {}
+        for category, data in RUBRIC.items():
+            if category in ["Assessment"]: # if multiple parts
+                feedback[category] = {}
+                for part in data:
+                    feedback[category][part] = {"comment": None, "score": None}
+            else:
+                feedback[category] = {"comment": None, "score": None}
+        evaluation["feedback"] = feedback
+
+    # Insert if new, either way record start time
+    if not st.session_state['started_time']:
+        update_evaluation("start", evaluation)
+        st.session_state['started_time'] = True
+
+    return interview, evaluation
 
 
 if st.session_state["stage"] == LOGIN_PAGE:
@@ -127,73 +157,49 @@ if st.session_state["stage"] == HUMAN_EVAL:
                 label += " " + eval_for_label["mark"]
                 label = emoji.emojize(label, language='alias')
             st.session_state["interviews_label:index"][label] = index
-        st.session_state["current_index"] = 0
+        st.session_state["current_index"] = None
         st.session_state["current_evaluation"] = None
-
-    # Get the list of interview labels for the selectbox
-    interview_labels = list(st.session_state["interviews_label:index"].keys())
-    
-    # Load the current interview based on view_index
-    interview_id = st.session_state["interview_list"][st.session_state["current_index"]]["_id"]
-    interview = COLLECTION_INTERVIEWS.find_one({"_id": interview_id})
-
-    # Load or create the evaluation for this interview
-    evaluation = COLLECTION_EVALUATIONS.find_one({"username": st.session_state["username"], "interview_info._id": interview_id})
-    if not evaluation:
-        # Create a new evaluation document
-        evaluation = {
-            "username": st.session_state["username"],
-            "interview_info": st.session_state["interview_list"][st.session_state["current_index"]],
-            "mark": "",
-            "times": []
-        }
-        # Record start time
-        record_time(evaluation)
-        # Initialize feedback structure
-        feedback = {}
-        for category, data in RUBRIC.items():
-            if category in ["Assessment"]: # if multiple parts
-                feedback[category] = {}
-                for part in data:
-                    feedback[category][part] = {"comment": None, "score": None}
-            else:
-                feedback[category] = {"comment": None, "score": None}
-        evaluation["feedback"] = feedback
-        # Save the new evaluation to the database
-        COLLECTION_EVALUATIONS.insert_one(evaluation)
-    
-    # Store the current evaluation in session state
-    st.session_state["current_evaluation"] = evaluation
+        st.session_state['started_time'] = False
 
     # Selectbox section
     layout11 = layout1[1].columns([1, 2, 1])
     
     # Function for selectbox change
     def on_select_change():
-        update_evaluation(st.session_state["current_evaluation"])
+        if st.session_state["current_evaluation"]:
+            update_evaluation("end", st.session_state["current_evaluation"])
         # Get the new index from the selected label
-        st.session_state["current_index"] = st.session_state["interviews_label:index"][st.session_state["selected_label"]]
+        st.session_state["current_index"] = st.session_state["interviews_label:index"][st.session_state["selected"]]
+        st.session_state['started_time'] = False
         
     # Create the selectbox
-    selected_label = layout11[1].selectbox(
+    # selectbox_index = st.session_state.get("current_index", None)
+    # previous = st.session_state.get("selected", None)
+    layout11[1].selectbox(
         "Select an interview:", 
         options=st.session_state["interviews_label:index"],
         index=st.session_state["current_index"],
         placeholder="Select Interview", 
         label_visibility="collapsed", 
-        key="selected_label",
+        key="selected",
         on_change=on_select_change
     )
-    
-    # The meat - display the evaluation form and capture updated feedback
-    evaluation["feedback"] = display_evaluation(interview, evaluation["feedback"])
-    
-    # Update session state with latest evaluation
-    st.session_state["current_evaluation"] = evaluation
+    # # If the user changed the selection, update evaluation
+    # if previous and selected != previous:
+    #     update_evaluation("end", st.session_state["current_evaluation"])
+    #     st.session_state["current_index"] = st.session_state["interviews_label:index"][selected]
+
+    if st.session_state["current_index"] is not None:
+        # Load/setup current interview and evaluation as needed
+        interview, evaluation = load_and_setup()
+        st.session_state["current_evaluation"] = evaluation
+        # Display the evaluation form and capture updated feedback
+        evaluation["feedback"] = display_evaluation(interview, evaluation["feedback"])
+        st.session_state["current_evaluation"] = evaluation
 
     # Function to handle dropdown navigation
     def navigate(direction):
-        update_evaluation(st.session_state["current_evaluation"])
+        update_evaluation("end", st.session_state["current_evaluation"])
         
         # Update the view index based on direction
         if direction == "next":
@@ -202,35 +208,38 @@ if st.session_state["stage"] == HUMAN_EVAL:
         elif direction == "back":
             st.session_state["current_index"] = max(0, st.session_state["current_index"] - 1)
 
+        st.session_state['started_time'] = False
+
     # Function to mark current evaluation
     def mark_evaluation(mark: str):
         st.session_state["current_evaluation"]["mark"] = mark
-        update_evaluation(st.session_state["current_evaluation"])
+        update_evaluation("mark", st.session_state["current_evaluation"])
         update_label(st.session_state["current_evaluation"], st.session_state["current_index"])
 
     # Function to save current evaluation
     def save_evaluation():
-        update_evaluation(st.session_state["current_evaluation"])
+        update_evaluation("save", st.session_state["current_evaluation"])
         
-    # Top navigation buttons
-    if layout11[0].button("Back", use_container_width=True, key="backtop"):
-        navigate("back")
-        st.rerun()
-    if layout11[2].button("Next", use_container_width=True, key="nexttop"):
-        navigate("next")
-        st.rerun()
-    
-    # Save button
-    layout12 = layout1[1].columns([1, 1, 1])
-    if layout12[0].button(emoji.emojize("Flag :triangular_flag_on_post:"), use_container_width=True, key="flagtop"):
-        mark_evaluation(":triangular_flag_on_post:")
-        st.rerun()
-    if layout12[1].button("Save", use_container_width=True, key="savetop"):
-        save_evaluation()
-        st.rerun()
-    if layout12[2].button(emoji.emojize("Done :white_check_mark:"), use_container_width=True, key="donetop"):
-        mark_evaluation(":white_check_mark:")
-        st.rerun()
+    if st.session_state["current_index"] is not None:
+        # Top navigation buttons
+        if layout11[0].button("Back", use_container_width=True, key="backtop"):
+            navigate("back")
+            st.rerun()
+        if layout11[2].button("Next", use_container_width=True, key="nexttop"):
+            navigate("next")
+            st.rerun()
+        
+        # Save button
+        layout12 = layout1[1].columns([1, 1, 1])
+        if layout12[0].button(emoji.emojize("Flag :triangular_flag_on_post:"), use_container_width=True, key="flagtop"):
+            mark_evaluation(":triangular_flag_on_post:")
+            st.rerun()
+        if layout12[1].button("Save", use_container_width=True, key="savetop"):
+            save_evaluation()
+            st.rerun()
+        if layout12[2].button(emoji.emojize("Done :white_check_mark:"), use_container_width=True, key="donetop"):
+            mark_evaluation(":white_check_mark:")
+            st.rerun()
 
     # # Bottom navigation buttons
     # layout2 = st.columns([2, 3, 2])
